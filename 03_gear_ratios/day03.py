ffcc60de4455450f61bed5f343dab317caacac37
Scriptim <me@scriptim.dev>
2023-12-06 22:31:24 +0100

    Add Python solution for day 3, part 1

---
+++ 03_gear_ratios/day03.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+def adjacent(number: tuple[int, int, int], symbol: tuple[str, int, int]) -> bool:
+    (num_val, num_row, num_col) = number
+    num_width = len(str(num_val))
+    (_, sym_row, sym_col) = symbol
+    return num_col - 1 <= sym_col <= num_col + num_width and abs(sym_row - num_row) <= 1
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as f:
+
+        # parse input
+        symbols = []  # (symbol, row, col)
+        numbers = []  # (value, row, col)
+        row = 0
+        while line := f.readline().strip():
+            reading_number = False
+            for col, char in enumerate(line):
+                if char == '.':
+                    # blank
+                    reading_number = False
+                elif char.isdigit():
+                    # number
+                    if reading_number:
+                        num_val, num_row, num_col = numbers[-1]
+                        numbers[-1] = (num_val * 10 + int(char), num_row, num_col)
+                    else:
+                        numbers.append((int(char), row, col))
+                        reading_number = True
+                else:
+                    # symbol
+                    symbols.append((char, row, col))
+                    reading_number = False
+            row += 1
+
+        # part 1
+        result = 0
+        for number in numbers:
+            if any(adjacent(number, symbol) for symbol in symbols):
+                result += number[0]
+        print(result)
