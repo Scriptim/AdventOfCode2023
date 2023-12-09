b7674c9d9fa17072c10664935604d66d166c2bda
Scriptim <me@scriptim.dev>
2023-12-09 09:12:49 +0100

    Add Python solution for day 9, part 1

---
+++ 09_mirage_maintenance/mirage_maintenance.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def find_next(sequence: list[int]) -> int:
+    if all(n == 0 for n in sequence):
+        return 0
+    deltas = [sequence[i] - sequence[i-1] for i in range(1, len(sequence))]
+    return sequence[-1] + find_next(deltas)
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as f:
+        # parse input
+        sequences = [[int(n) for n in line.strip().split()] for line in f.readlines()]
+
+        # part 1
+        result = sum(find_next(sequence) for sequence in sequences)
+        print(result)
