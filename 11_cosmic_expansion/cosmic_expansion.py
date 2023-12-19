9801fe25a6b21da26579c17aa2fc752553d85dd9
Scriptim <me@scriptim.dev>
2023-12-19 13:54:40 +0100

    Add Python solution for day 11, part 2

--- 11_cosmic_expansion/cosmic_expansion.py
+++ 11_cosmic_expansion/cosmic_expansion.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+def distance(galaxy1: (int, int), galaxy2: (int, int), expand_rows: [int], expand_columns: [int], expand_factor: int) -> int:
+    crossed_rows = [row for row in expand_rows if min(y1, y2) < row < max(y1, y2)]
+    crossed_columns = [col for col in expand_columns if min(x1, x2) < col < max(x1, x2)]
+    crossed_total = len(crossed_rows) + len(crossed_columns)
+
+    distance = abs(x1 - x2) + abs(y1 - y2) - crossed_total
+    distance += expand_factor * crossed_total
+    return distance
+
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         # parse input
@@ -13,14 +22,19 @@ if __name__ == '__main__':
                 if char == '#':
                     galaxies.append((x, y))
 
-        # part 1
         expand_rows = set(range(0, height)) - {y for _, y in galaxies}
         expand_columns = set(range(0, width)) - {x for x, _ in galaxies}
+
+        # part 1
         result = 0
         for i, (x1, y1) in enumerate(galaxies):
             for x2, y2 in galaxies[i+1:]:
-                distance = abs(x1 - x2) + abs(y1 - y2)
-                distance += len([row for row in expand_rows if min(y1, y2) < row < max(y1, y2)])
-                distance += len([col for col in expand_columns if min(x1, x2) < col < max(x1, x2)])
-                result += distance
+                result += distance((x1, y1), (x2, y2), expand_rows, expand_columns, 2)
+        print(result)
+
+        # part 2
+        result = 0
+        for i, (x1, y1) in enumerate(galaxies):
+            for x2, y2 in galaxies[i+1:]:
+                result += distance((x1, y1), (x2, y2), expand_rows, expand_columns, 1000000)
         print(result)
